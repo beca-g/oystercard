58b0a66c1cb1a029a3de3516ad1cf05bb5a2949c
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-05 16:25:30 +0100

    RED: Added test to check that new oystercards have a balance of 0 when initialized
    F
    
    Failures:
    
      1) Oystercard initialized cards freshly initialized cards should have a balance of 0
         Failure/Error: expect(subject.balance).to eq 0
    
         NoMethodError:
           undefined method `balance' for #<Oystercard:0x00007fbed89a2f38>
         # ./spec/oystercard_spec.rb:5:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00233 seconds (files took 0.09511 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/oystercard_spec.rb:4 # Oystercard initialized cards freshly initialized cards should have a balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
 require './lib/oystercard'
 describe Oystercard do
+  describe "initialized cards" do
+    it "freshly initialized cards should have a balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end 
 end
